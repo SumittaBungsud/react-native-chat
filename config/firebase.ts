675fad25a62761f945b8430dcb951af25e8750b7
SumittaBungsud <sumitta.hct@gmail.com>
2024-04-08 01:33:35 +0700

    update firebase config

--- config/firebase.ts
+++ config/firebase.ts
@@ -2,25 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-interface FirebaseProps {
-  apiKey: string;
-  authDomain: string;
-  projectId: string;
-  storageBucket: string;
-  messagingSenderId: string;
-  appId: string;
-}
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDDwYFV4vKjRzJVWNz8Wz_efL5xB6CQ3Fc",
-//   authDomain: "rn-chatapp-31eba.firebaseapp.com",
-//   projectId: "rn-chatapp-31eba",
-//   storageBucket: "rn-chatapp-31eba.appspot.com",
-//   messagingSenderId: "991578267247",
-//   appId: "1:991578267247:web:172ef429afc8f3287b779a",
-// }
-
-const firebaseConfig = <FirebaseProps>{
+const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_API_KEY,
   authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
   projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
